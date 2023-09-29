365903fddbdb59c5d8579e2e56ade0d17735f942
FeijooCiro <128099617+FeijooCiro@users.noreply.github.com>
2023-09-29 18:52:16 -0300

    Me olvide el puerto

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ function generateFigure(frase) {
     return resultado;
 }
 
-const PORT = process.env.PORT || 3000;
+/*const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor en funcionamiento en el puerto ${PORT}`);
-});
+});*/
